b2a90048e3268816d411f3796d24ef83bb7c348c
Tristan Blackwell <32044892+TristanBlackwell@users.noreply.github.com>
2025-08-06 21:01:44 +0100

    chore: add biomejs

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import tailwindcss from "@tailwindcss/vite"
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
-import viteReact from '@vitejs/plugin-react';
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   server: {
@@ -14,4 +14,4 @@ export default defineConfig({
     viteReact(),
     tailwindcss(),
   ],
-})
+});
